56840bd3277075fc120630d7c68736cba1ff6369
tgray017 <tgray017@gmail.com>
2018-09-19 00:22:05 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match(/^[aeiou]/)
-    return true
-  end
+  return true if word.match(/^[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
